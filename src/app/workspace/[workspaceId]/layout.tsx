f845b290c117833186da7210bb0c55d0b8821d64
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-10-18 16:40:55 +0530

    Workspace Header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -2,6 +2,12 @@
 
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -13,6 +19,20 @@ const WorkspaceLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="ca-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
         {children}
       </div>
     </div>
