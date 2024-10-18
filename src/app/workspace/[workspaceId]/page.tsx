8711654a6c1d620722f873af044bcb0961a4471f
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-10-18 09:15:08 +0530

    Sidebar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -5,8 +5,8 @@ import { useWorkspaceId } from "@/hooks/use-workspacd-id";
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkSpace({ id: workspaceId });
-  return <div>ID : {JSON.stringify(data)}</div>;
+
+  return <div>Workspace id page</div>;
 };
 
 export default WorkspaceIdPage;
