768db25b5897abdbdb29d1b90495e1e043ba9c75
Nirmit Kotadiya <kotadiyanirmit24@gmail.com>
2024-10-29 16:06:14 +0530

    Cosmetic Improvements

--- src/components/emoji-popover.tsx
+++ src/components/emoji-popover.tsx
@@ -1,5 +1,4 @@
-import Picker from "@emoji-mart/react";
-import data from "@emoji-mart/data";
+import EmojiPicker, { type EmojiClickData } from "emoji-picker-react";
 
 import {
   Popover,
@@ -18,7 +17,7 @@ import { useState } from "react";
 interface EmojiPopoverProps {
   children: React.ReactNode;
   hint?: string;
-  onEmojiSelect: (emoji: any) => void;
+  onEmojiSelect: (value: string) => void;
 }
 
 export const EmojiPopover = ({
@@ -29,13 +28,13 @@ export const EmojiPopover = ({
   const [popoverOpen, setPopoverOpen] = useState(false);
   const [tooltipOpen, setTooltipOpen] = useState(false);
 
-  const onSelect = (emoji: any) => {
-    onEmojiSelect(emoji);
+  const onSelect = (value: EmojiClickData) => {
+    onEmojiSelect(value?.emoji);
     setPopoverOpen(false);
 
-    // setTimeout(() => {
-    //   setTooltipOpen(false);
-    // }, 500);
+    setTimeout(() => {
+      setTooltipOpen(false);
+    }, 500);
   };
 
   return (
@@ -59,7 +58,7 @@ export const EmojiPopover = ({
           </TooltipContent>
         </Tooltip>
         <PopoverContent className="p-0 w-full border-none shadow-none">
-          <Picker data={data} onEmojiSelect={onSelect} />
+          <EmojiPicker onEmojiClick={onSelect} />
         </PopoverContent>
       </Popover>
     </TooltipProvider>
